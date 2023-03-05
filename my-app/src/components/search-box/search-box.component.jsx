b557310e74668851abc0d17c918bbdb9989678f2
SamuelList <list.samuel@gmail.com>
2023-03-05 12:20:03 -0600

    first commit

---
+++ my-app/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+import './search-box.styles.css';
+
+class SearchBox extends Component {
+  render() {
+    const { onChangeHandler } = this.props;
+
+    return (
+      <input
+        className={`search-box ${this.props.className}`}
+        type="search"
+        placeholder={this.props.placeholder}
+        onChange={onChangeHandler}
+      />
+    );
+  }
+}
+
+export default SearchBox;
