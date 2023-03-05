15ec0ce706289050391e024df1111742284968a7
SamuelList <list.samuel@gmail.com>
2023-03-05 15:30:30 -0600

    added title and css for title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        
+        <h1 className="app-title">The People</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
           placeholder='Search People'
